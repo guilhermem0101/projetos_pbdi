19e967162cec20e3152c9b289cf4f31d8ba8321f
Guilherme Matheus <caiolimacruz@gmail.com>
2024-06-11 12:01:44 -0300

    feat(ap013): cria procedure numero estudantes

--- projeto_csv_sp_fn/projeto_csv.sql
+++ projeto_csv_sp_fn/projeto_csv.sql
@@ -7,3 +7,51 @@ CREATE TABLE tb_student_prediction(
 	notes INT,
 	grade INT
 );
+
+--Exibe o número de estudantes maiores de idade
+DO
+$$
+DECLARE
+    cur_student REFCURSOR;
+    idade INT;
+    contador INT := 0;
+BEGIN
+    OPEN cur_student FOR 
+    SELECT age FROM tb_student_prediction;
+
+    LOOP
+        FETCH cur_student INTO idade;
+        IF idade > 18 THEN
+            contador := contador + 1;
+        ELSE  RAISE NOTICE '';
+        END IF;
+        EXIT WHEN NOT FOUND;
+        
+    END LOOP;
+    RAISE NOTICE '%', contador;
+END;
+$$
+
+-- Exibe o percentual de estudantes de cada sexo.
+DO
+$$
+DECLARE
+    cur_student REFCURSOR;
+    genero INT;
+    contador INT := 0;
+BEGIN
+    OPEN cur_student FOR 
+    SELECT gender FROM tb_student_prediction;
+
+    LOOP
+        FETCH cur_student INTO idade;
+        IF genero = 1 THEN
+            contador := contador + 1;
+        ELSE  RAISE NOTICE '';
+        END IF;
+        EXIT WHEN NOT FOUND;
+        
+    END LOOP;
+    RAISE NOTICE '%', contador;
+END;
+$$
