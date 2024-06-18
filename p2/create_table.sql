7691c3615b7b1c6766420b5a613de159d8673959
Guilherme Matheus <caiolimacruz@gmail.com>
2024-06-18 09:41:48 -0300

    feat(p2): tabela para importação dos dados

---
+++ p2/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    age INT,
+    gender INT,
+    mother_edu INT,
+    father_edu INT,
+    scholarship INT,
+    work INT,
+    study_hrs INT,
+    salary INT,
+    grade INT
+);
