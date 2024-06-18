b51be7f96b0a9a0bba9b2de486d7cf43f8e7a4a8
Guilherme Matheus <caiolimacruz@gmail.com>
2024-06-18 10:33:12 -0300

    feat(p2):aprovados com pais phds

--- p2/p2.sql
+++ p2/p2.sql
@@ -10,3 +10,25 @@ CREATE TABLE tb_estudantes(
 );
 
 
+--Escreva procedure que exibe o número de alunos aprovados e cujos pais são phds
+CREATE OR REPLACE PROCEDURE sp_alunos_aprovados(
+	 OUT contagem INT
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+	SELECT COUNT(*)
+	INTO contagem
+	FROM tb_estudantes
+	WHERE father_edu = 6 and mother_edu = 6 and grade > 0;
+END;
+$$
+
+DO $$
+DECLARE
+contagem INT;
+BEGIN
+	CALL sp_alunos_aprovados(contagem);
+	RAISE NOTICE 'Alunos aprovados com pais PHDs : %', contagem;
+END;
+$$
+
